4bf4edb183245c86b9da364890a3480ed86c8988
Chen <sddd1234567@gmail.com>
2019-11-10 05:12:42 +0800

    建立幾個特殊prop的Prefab(數值待調整)、新增特效

---
+++ Assets/Effects/Scripts/RingJitter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingJitter : MonoBehaviour
+{
+
+    // Use this for initialization
+    float count = 0;
+    float horizonCount = 0;
+    Vector2 defaultPosition;
+
+    RectTransform rectTransform;
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        defaultPosition = rectTransform.anchoredPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime * 100;
+        horizonCount += Time.deltaTime;
+        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, Mathf.Sin(count) * 15 + defaultPosition.y);
+        // rectTransform.rotation = Quaternion.Euler(rectTransform.rotation.x, rectTransform.rotation.y, count);
+    }
+}
---
+++ Assets/Effects/Scripts/Scale.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+
+    [SerializeField]
+    int loopTime;
+    [SerializeField]
+    float[] scaleRate;
+    // Use this for initialization
+    [SerializeField]
+    float time;
+    void Start()
+    {
+        CoroutineQueue queue = CoroutineUtility.GetInstance().Do();
+        for (int i = 0; i < loopTime; i++)
+        {
+            for (int a = 0; a < scaleRate.Length; a++)
+            {
+                queue.ScaleUI(gameObject, Vector3.one * scaleRate[a], time);
+            }
+        }
+        queue.Go();
+    }
+
+}
