9247a4c234d18d25be20a3b16c5985149b295517
McGodamn <t7214948ttt@gmail.com>
2019-11-09 17:59:15 +0800

    Add Drag Scene

---
+++ Assets/View/Script/DragObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour {
+
+	RectTransform rectTransform;
+	Vector2 originPos;
+
+	float InputZ;
+	// Use this for initialization
+	void Start () {
+        InputZ = -Camera.main.transform.position.z;
+        rectTransform = GetComponent<RectTransform>();
+	}
+
+	public void OnBeginDrag()
+	{
+		originPos = transform.position;
+	}
+	
+	public void OnDrag()
+    {
+        var InputPos = Input.mousePosition;
+        InputPos.z = InputZ;
+        rectTransform.position = Camera.main.ScreenToWorldPoint(InputPos);
+	}
+
+	public void OnEndDrag()
+	{
+        rectTransform.position = originPos;
+	}
+}
